a9601ca4afe4f892455997e5c19a07474ad568ef
Agata Filho <agata78@gmail.com>
2016-06-04 10:31:11 -0400

    Fix flask name: __name__, not "__name__".
    Reformat a bit, as well.

--- main.py
+++ main.py
@@ -5,15 +5,14 @@ import statsd
 import stat_server
 
 def main():
-    app = Flask("__name__")
+    app = Flask(__name__)
 
     @app.route("/page/<id>")
     def display_message(id):
         stat_server.users_stat()
-        return "Page ID is " + str(id) + "!"
+        return "Page ID is {} !".format(id)
 
-    app.debug = True
-    app.run()
+    app.run(debug=True)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
